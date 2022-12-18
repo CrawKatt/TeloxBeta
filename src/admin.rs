d71674357a6f439f4843e451d98c39940d2eba66
Shinobi325 <man849916@gmail.com>
2022-12-18 20:25:05 -0300

    Refactorización adicional de TeloxBeta

--- src/admin.rs
+++ src/admin.rs
@@ -15,67 +15,52 @@ use teloxide_core::types::{ChatMemberStatus, Message};
 
 // Banear a un usuario respondiendo un mensaje
 pub async fn ban_user(bot: comandos::MyBot, msg: Message) -> ResponseResult<()> {
-    match msg.reply_to_message() {
+    let user = msg.reply_to_message().and_then(|replied| replied.from()).unwrap();
+    println!("Usuario a banear: {}", user.id);
 
-        Some(replied) => {
+    let chat_id = msg.chat.id;
+    println!("Chat id: {}", chat_id);
 
-            let user = msg.reply_to_message().unwrap().from().unwrap();
-            println!("Info : {:?}", user);
+    let username_user = user.username.clone().unwrap_or_default();
+    println!("Username: {}", username_user);
 
-            let chat = msg.chat.id;
-            println!("Chat ID : {:?}", chat);
+    let chat_member = bot.get_chat_member(chat_id, msg.from().unwrap().id).await?;
 
-            let usuario = user.clone().first_name;
-            println!("Nombre : {:?}", usuario);
+    let is_admin_or_owner = chat_member.status().is_administrator() || chat_member.status().is_owner();
+    println!("Es admin o owner: {}", is_admin_or_owner);
 
-            let username_user = user.clone().username;
-            println!("@username : {:?}", username_user);
+    if is_admin_or_owner {
+        bot.delete_message(chat_id, msg.id).await?;
+        bot.ban_chat_member(chat_id, user.id).await?;
+        bot.send_message(chat_id, format!("@{} ha sido baneado", username_user), ).await?;
 
-            let chat_member = bot.get_chat_member(msg.chat.id, msg.from().unwrap().id).await?;
+        let mut rng: StdRng = SeedableRng::from_entropy();
+        let random_number = rng.gen_range(0..=14);
 
-            let user_id = chat_member.user.id;
-            println!("ID usuario : {:?}", user_id);
+        let file_names = [
+            "1.gif", "2.gif", "3.gif", "4.gif", "5.gif",
+            "6.gif", "7.gif", "8.gif", "9.gif", "10.gif",
+            "11.gif", "12.mp4", "13.mp4", "14.mp4", "15.mp4",
+        ];
 
-            if chat_member.status() == ChatMemberStatus::Administrator || chat_member.status() == ChatMemberStatus::Owner || chat_member.status() == ChatMemberStatus::Administrator {
-                bot.delete_message(msg.chat.id, msg.id).await?;
-                bot.ban_chat_member(msg.chat.id, replied.from().unwrap().id).await?;
-                bot.send_message(msg.chat.id, format!("@{} ha sido baneado", username_user.unwrap())).await?;
+        let get_file_name = |index: usize| -> &'static str {
+            file_names.get(index).unwrap_or_else(|| file_names.last().unwrap())
+        };
 
-                let mut rng : StdRng = SeedableRng::from_entropy();
-                let random_number = rng.gen_range(0..=15);
-                println!("Resultado : {}", random_number);
+        let file_path = format!("./assets/ban/{}", get_file_name(random_number));
 
-                match random_number {
-                    0  => bot.send_animation(msg.chat.id, InputFile::file("./assets/ban/1.gif" )).await?,
-                    1  => bot.send_animation(msg.chat.id, InputFile::file("./assets/ban/2.gif" )).await?,
-                    2  => bot.send_animation(msg.chat.id, InputFile::file("./assets/ban/3.gif" )).await?,
-                    3  => bot.send_animation(msg.chat.id, InputFile::file("./assets/ban/4.gif" )).await?,
-                    4  => bot.send_animation(msg.chat.id, InputFile::file("./assets/ban/5.gif" )).await?,
-                    5  => bot.send_animation(msg.chat.id, InputFile::file("./assets/ban/6.gif" )).await?,
-                    6  => bot.send_animation(msg.chat.id, InputFile::file("./assets/ban/7.gif" )).await?,
-                    7  => bot.send_animation(msg.chat.id, InputFile::file("./assets/ban/8.gif" )).await?,
-                    8  => bot.send_animation(msg.chat.id, InputFile::file("./assets/ban/9.gif" )).await?,
-                    9  => bot.send_animation(msg.chat.id, InputFile::file("./assets/ban/10.gif")).await?,
-                    10 => bot.send_animation(msg.chat.id, InputFile::file("./assets/ban/11.gif")).await?,
-                    11 => bot.send_video(msg.chat.id, InputFile::file("./assets/ban/12.mp4")).await?,
-                    12 => bot.send_video(msg.chat.id, InputFile::file("./assets/ban/13.mp4")).await?,
-                    13  => bot.send_video(msg.chat.id, InputFile::file("./assets/ban/14.mp4")).await?,
-                    _  => bot.send_video(msg.chat.id, InputFile::file("./assets/ban/15.mp4")).await?,
-                };
-            } else {
-                bot.delete_message(msg.chat.id, msg.id).await?;
-                bot.send_message(msg.chat.id, "No tienes permisos para banear a un usuario\\.").await?;
-            }
+        if file_path.ends_with(".gif") {
+            bot.send_animation(chat_id, InputFile::file(file_path)).await?;
+        } else {
+            bot.send_video(chat_id, InputFile::file(file_path)).await?;
         }
-
-        None => {
-            bot.delete_message(msg.chat.id, msg.id).await?;
-            bot.send_message(msg.chat.id, "Usa este comando respondiendo a otro mensaje").await?;
-        }
-
+    } else {
+        bot.delete_message(chat_id, msg.id).await?;
+        bot.send_message(chat_id, "No tienes permisos para usar este comando",).await?;
     }
     Ok(())
 }
+
 
 pub async fn unban_user(bot: comandos::MyBot, msg: Message) -> ResponseResult<()> {
     match msg.reply_to_message() {
