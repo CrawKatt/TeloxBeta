8a82bf9f49ee1479b87a3bd4c64085f3ce459575
CrawKatt <man849916@gmail.com>
2023-02-22 18:05:30 -0300

    admin.rs renombrado a testing.rs que incluye comandos experimentales

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -1,6 +1,6 @@
 // Imports de archivos del bot
 // Imports of bot files
-pub mod admin;
+pub mod testing;
 pub mod handler;
 pub mod funciones;
 pub mod admin_commands;
@@ -8,8 +8,8 @@ pub mod fun_commands;
 
 // uso de archivos del bot
 // Use of bot files
-pub use admin::*;
+pub use testing::*;
 pub use handler::*;
 pub use funciones::*;
-pub use crate::commands::admin_commands::*; // Llama a la carpeta admin_commands >> calls the admin_commands folder and mod.rs into the admin_commands folder
-pub use crate::commands::fun_commands::*; // Llama a la carpeta fun_commands >> calls the fun_commands folder and mod.rs into the fun_commands folder
+pub use admin_commands::*; // Llama a la carpeta admin_commands >> calls the admin_commands folder and mod.rs into the admin_commands folder
+pub use fun_commands::*; // Llama a la carpeta fun_commands >> calls the fun_commands folder and mod.rs into the fun_commands folder
