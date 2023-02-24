34eb236659908278c90346dfe2b897c231c475ab
CrawKatt <man849916@gmail.com>
2023-02-24 14:54:40 -0300

    Se agregó Explicación sobre como funciona el sistema de envío de multimedia aleatorio tanto en Inglés como en Español
    Added Explanation on how the random multimedia delivery system works in both English and Spanish

--- src/commands/fun_commands/meme.rs
+++ src/commands/fun_commands/meme.rs
@@ -26,35 +26,51 @@ pub async fn send_random_meme(bot: Bot, msg: Message) -> ResponseResult<()> {
             .unwrap_or(&file_names[0])
     };
 
+    /*Español/Spanish*/
     // generamos un numero aleatorio
+
+    /*Inglés/English*/
+    // generate a random number
     let mut rng: StdRng = SeedableRng::from_entropy();
     let random_number = rng.gen_range(0..=file_names.len() - 1);
 
+    /*Inglés/English*/
     // imprimimos el resultado en consola con fines de debug
+    // print the result in the console for debugging purposes
     println!("Resultado : {}", random_number);
 
     // borramos el mensaje original
-    bot.delete_message(msg.chat.id, msg.id).await?;
+    //bot.delete_message(msg.chat.id, msg.id).await?;
 
+    /*Español/Spanish*/
     // Utiliza la función `format!` para crear una cadena de caracteres que contiene una ruta de archivo.
     // La ruta de archivo se compone de la cadena "./assets/memes/" seguida del nombre de archivo obtenido de la función `get_file_name`.
     // El argumento proporcionado a `get_file_name` es `random_number`, que es un número aleatorio generado previamente.
+
+    /*Inglés/English*/
+    // Use the `format`! function to create a string that contains a file path.
+    // The file path consists of the string "./assets/memes/" followed by the file name obtained from the `get_file_name` function.
+    // The argument provided to `get_file_name` is `random_number`, which is a randomly generated number previously.
     let file_path = format!("./assets/memes/{}", get_file_name(random_number));
 
+    /*Español/Spanish*/
     // Utiliza la función `Path::new` para crear un objeto `Path` a partir de la cadena de caracteres `file_path`.
     // Luego, se llama al método `is_file` del objeto `Path` para verificar si la ruta de archivo apunta a un archivo existente.
-    if Path::new(&file_path).is_file() {
+
+    /*Inglés/English*/
+    // Use the `Path::new` function to create a `Path` object from the string `file_path`.
+    // Then, call the `is_file` method of the `Path` object to check if the file path points to an existing file.
+
+    if Path::new(&file_path).is_file() { // If the file path points to an existing file, check if the file name ends with ".mp4".
         // Si la ruta de archivo apunta a un archivo existente, se verifica si el nombre de archivo termina con ".mp4".
-        if file_path.ends_with(".mp4") {
+        if file_path.ends_with(".mp4") { // If the file name ends with ".mp4", send the video file to the chat using the `send_video` method of the `bot` object.
             // Si el nombre de archivo termina con ".mp4", se envía el archivo de video al chat utilizando el método `send_video` del objeto `bot`.
             // El argumento proporcionado a `send_video` es un objeto `InputFile` que se crea a partir de la ruta de archivo.
-            bot.send_video(msg.chat.id, InputFile::file(&file_path))
-                .await?;
-        } else {
+            bot.send_video(msg.chat.id, InputFile::file(&file_path)).reply_to_message_id(msg.id).await?; // The argument provided to `send_video` is an `InputFile` object that is created from the file path.
+        } else { // If the file name does not end with ".mp4", assume it is an image file and send it to the chat using the `send_photo` method of the `bot` object.
             // Si el nombre de archivo no termina con ".mp4", se asume que es un archivo de imagen y se envía al chat utilizando el método `send_photo` del objeto `bot`.
             // El argumento proporcionado a `send_photo` es un objeto `InputFile` que se crea a partir de la ruta de archivo.
-            bot.send_photo(msg.chat.id, InputFile::file(&file_path))
-                .await?;
+            bot.send_photo(msg.chat.id, InputFile::file(&file_path)).reply_to_message_id(msg.id).await?; // The argument provided to `send_photo` is an `InputFile` object that is created from the file path.
         }
     }
 
