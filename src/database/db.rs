b18799be6a5d1984647a0e99a848e4dc5b8955e9
CrawKatt <man849916@gmail.com>
2023-02-27 19:36:33 -0300

    cambios irrelevantes

--- src/database/db.rs
+++ src/database/db.rs
@@ -1,8 +1,7 @@
 use crate::database::*;
 
 pub async fn conectar() -> Result<(), Box<dyn Error>> {
-    let client_uri =
-        env::var("MONGODB_URI").expect("\nERROR AL CONECTAR A LA BASE DE DATOS DE MONGODB\n¡Debes establecer la variable de entorno MONGODB_URI!");
+    let client_uri = env::var("MONGODB_URI").expect("\nERROR AL CONECTAR A LA BASE DE DATOS DE MONGODB\n¡Debes establecer la variable de entorno MONGODB_URI!");
 
     let options = ClientOptions::parse(&client_uri).await?;
     let client = Client::with_options(options)?;
