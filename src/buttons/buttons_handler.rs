2b464814c5f993dfa2cbb81e5bcba2f01884b74b
CrawKatt <man849916@gmail.com>
2023-03-03 18:06:52 -0300

    Se agregaron funciones para ciertos botones y se ha organizado el código (No terminado)

--- src/buttons/buttons_handler.rs
+++ src/buttons/buttons_handler.rs
@@ -64,17 +64,38 @@ pub fn make_rust_keyboard() -> InlineKeyboardMarkup {
     InlineKeyboardMarkup::new(keyboard)
 }
 
-pub async fn inline_query_handler(bot: Bot, q: InlineQuery, ) -> Result<(), Box<dyn Error + Send + Sync>> {
+pub fn make_settings_keyboard() -> InlineKeyboardMarkup {
+    let mut keyboard: Vec<Vec<InlineKeyboardButton>> = vec![];
+    let settings = ["AntiSpam", "Advertencias", "Reglamento", "Volver"];
 
-    let buttons = InlineQueryResultArticle::new(
-        "0",
-        "Botones",
-        InputMessageContent::Text(InputMessageContentText::new("Botones")),
-    ).reply_markup(make_main_keyboard());
+    for setting in settings.chunks(3) {
+        let row = setting.iter().map(|&option| {
+            match option {
+                "Volver" => InlineKeyboardButton::callback(option.to_owned(), "back_to_main_keyboard".to_owned()),
+                _ => InlineKeyboardButton::callback(option.to_owned(), option.to_owned())
+            }
+        }).collect();
+        keyboard.push(row);
+    }
 
-    bot.answer_inline_query(q.id, vec![buttons.into()]).await?;
+    InlineKeyboardMarkup::new(keyboard)
+}
 
-    Ok(())
+pub fn make_about_keyboard() -> InlineKeyboardMarkup {
+    let mut keyboard: Vec<Vec<InlineKeyboardButton>> = vec![];
+    let about = ["GitHub", "Volver"];
+
+    for option in about.chunks(3) {
+        let row = option.iter().map(|&option| {
+            match option {
+                "Volver" => InlineKeyboardButton::callback(option.to_owned(), "back_to_main_keyboard".to_owned()),
+                _ => InlineKeyboardButton::callback(option.to_owned(), option.to_owned())
+            }
+        }).collect();
+        keyboard.push(row);
+    }
+
+    InlineKeyboardMarkup::new(keyboard)
 }
 
 pub fn make_donate_keyboard() -> InlineKeyboardMarkup {
@@ -111,9 +132,39 @@ pub fn make_language_keyboard() -> InlineKeyboardMarkup {
     InlineKeyboardMarkup::new(keyboard)
 }
 
+pub fn make_help_keyboard() -> InlineKeyboardMarkup {
+    let mut keyboard: Vec<Vec<InlineKeyboardButton>> = vec![];
+    let languages = ["Comandos", "Guía", "Soporte", "Volver"];
+
+    for language in languages.chunks(3) {
+        let row = language.iter().map(|&option| {
+            match option {
+                "Volver" => InlineKeyboardButton::callback(option.to_owned(), "back_to_main_keyboard".to_owned()),
+                _ => InlineKeyboardButton::callback(option.to_owned(), option.to_owned())
+            }
+        }).collect();
+        keyboard.push(row);
+    }
+
+    InlineKeyboardMarkup::new(keyboard)
+}
+
 pub fn make_back_button_keyboard() -> InlineKeyboardMarkup {
     let keyboard: Vec<Vec<InlineKeyboardButton>> = vec![];
     InlineKeyboardMarkup::new(keyboard)
+}
+
+pub async fn inline_query_handler(bot: Bot, q: InlineQuery, ) -> Result<(), Box<dyn Error + Send + Sync>> {
+
+    let buttons = InlineQueryResultArticle::new(
+        "0",
+        "Botones",
+        InputMessageContent::Text(InputMessageContentText::new("Botones")),
+    ).reply_markup(make_main_keyboard());
+
+    bot.answer_inline_query(q.id, vec![buttons.into()]).await?;
+
+    Ok(())
 }
 
 pub async fn callback_handler(bot: Bot, q: CallbackQuery) -> Result<(), Box<dyn Error + Send + Sync>> {
@@ -172,7 +223,7 @@ pub async fn callback_handler(bot: Bot, q: CallbackQuery) -> Result<(), Box<dyn 
         Some("Ajustes") => {
             if let Some(Message { id, chat, .. }) = q.message {
                 let text = "Elige una opción:".to_owned();
-                let keyboard = make_main_keyboard();
+                let keyboard = make_settings_keyboard();
                 bot.edit_message_text(chat.id, id, text).reply_markup(keyboard).await?;
             }
         }
@@ -188,16 +239,20 @@ pub async fn callback_handler(bot: Bot, q: CallbackQuery) -> Result<(), Box<dyn 
         Some("Acerca de") => {
             if let Some(Message { id, chat, .. }) = q.message {
                 let text = "Elige una opción:".to_owned();
-                let keyboard = make_main_keyboard();
+                let keyboard = make_about_keyboard();
                 bot.edit_message_text(chat.id, id, text).reply_markup(keyboard).await?;
             }
         }
 
-        Some("Comandos") => {
-            if let Some(Message { id, chat, .. }) = q.message {
-                let text = "Elige una opción:".to_owned();
-                let keyboard = make_main_keyboard();
-                bot.edit_message_text(chat.id, id, text).reply_markup(keyboard).await?;
+        Some("GitHub") => {
+            if let Some(Message { id, chat, ..}) = q.message {
+                let text =
+                    "GitHub: \nhttps://github.com/CrawKatt \
+                    \n\nRepositorio (Estable) del Bot: \nhttps://github.com/CrawKatt/TeloxBot \
+                    \n\nRepositorio (Beta) del Bot: \nhttps://github.com/CrawKatt/TeloxBeta"
+                        .to_owned();
+                let keyboard = make_back_button_keyboard();
+                bot.edit_message_text(chat.id, id, text).reply_markup(keyboard).parse_mode(ParseMode::Html).await?;
             }
         }
 
@@ -213,14 +268,6 @@ pub async fn callback_handler(bot: Bot, q: CallbackQuery) -> Result<(), Box<dyn 
             if let Some(Message { id, chat, .. }) = q.message {
                 let text = "Elige tu idioma:".to_owned();
                 let keyboard = make_language_keyboard();
-                bot.edit_message_text(chat.id, id, text).reply_markup(keyboard).await?;
-            }
-        }
-
-        Some("Novedades") => {
-            if let Some(Message { id, chat, .. }) = q.message {
-                let text = "Elige una opción:".to_owned();
-                let keyboard = make_main_keyboard();
                 bot.edit_message_text(chat.id, id, text).reply_markup(keyboard).await?;
             }
         }
