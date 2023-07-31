a366a902c062660cfed619eb327aa734298b8218
CrawKatt <man849916@gmail.com>
2023-07-31 14:15:13 -0400

    Refactor main.rs for user mention and chat member handling
    Removed unused code for the `MentionUser` trait and the `UserIdMention` struct. Replaced the MarkdownV2 parse mode with ParseMode::Html. Reworked the `chat_member_welcome` function to handle both chat member entrance and exit using `ChatMemberUpdated`s status. The previous implementation was redundant and hard to maintain, this change will streamline the code and improve maintainability.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-use teloxide::utils::html;
-// Import the commands and database modules
 use crate::utils::dependencies::*;
 pub mod buttons;
 pub mod commands;
@@ -8,48 +6,19 @@ pub mod utils;
 
 type MemberResult = Result<(), Box<dyn Error + Send + Sync>>;
 
-struct UserIdMention {}
-
-pub trait MentionUser {
-    #[must_use]
-    fn link(url: &str, text: &str) -> String {
-        format!("<a href=\"{url}\">{text}</a>")
-    }
-
-    #[must_use]
-    fn mention_user(user_id: UserId, text: &str) -> String {
-        let url = format!("tg://user?id={user_id}");
-        Self::link(&url, text)
-    }
-}
-
-impl MentionUser for UserIdMention {
-    fn mention_user(user_id: UserId, text: &str) -> String {
-        let url = format!("tg://user?id={user_id}");
-        Self::link(&url, text)
-    }
-}
-
-// Main function that starts the bot
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    // Initialize the logging environment for Teloxide
     pretty_env_logger::init();
     log::info!("Iniciando Bot...");
-
-    // Load environment variables from a .env file
     dotenv().ok();
 
     // Connect to the database
     //conectar().await.expect("Error al conectar con la Base de Datos");
 
-    // Load the init message from a txt file
     let init_message = include_str!("init_message.txt");
-
-    // Print a message to the console to indicate that the bot has started
     println!("{init_message}\n");
 
-    let bot = teloxide::Bot::from_env().parse_mode(MarkdownV2);
+    let bot = teloxide::Bot::from_env().parse_mode(ParseMode::Html);
     let handler = dptree::entry()
         .inspect(|u: Update| {
             println!("{u:#?}");
@@ -61,47 +30,49 @@ async fn main() -> Result<(), Box<dyn Error>> {
             Update::filter_chat_member()
                 .filter(|m: ChatMemberUpdated| {
                     m.old_chat_member.kind.is_left() && m.new_chat_member.kind.is_present()
+                        || m.old_chat_member.kind.is_present() && m.new_chat_member.kind.is_left()
                 })
                 .endpoint(chat_member_welcome),
-        )
-        .branch(
-            Update::filter_chat_member()
-                .filter(|m: ChatMemberUpdated| {
-                    m.new_chat_member.kind.is_left() && m.old_chat_member.kind.is_present()
-                })
-                .endpoint(chat_member_goodbye),
         );
 
+    // We create a dispatcher for our bot
     Dispatcher::builder(bot.clone(), handler).enable_ctrlc_handler().build().dispatch().await;
 
     Ok(())
 }
 
-// TODO: Mencionar por First_Name a los usuarios que se unan al grupo
+/// Welcome Function
+/// We use ChatMemberUpdated instead of Message for our function because
+/// Chat member updates != messages
 async fn chat_member_welcome(bot: Bot, chat_member: ChatMemberUpdated) -> MemberResult {
+    // We use this variable for get the user
     let user = chat_member.new_chat_member.user;
+
+    // We use this variable for get the user_id
+    let user_id = chat_member.old_chat_member.user.id;
+
+    // We use this variable for get the group name
     let telegram_group_name = chat_member.chat.title().unwrap_or("");
-    let user_id = chat_member.old_chat_member.user.id;
+
+    // We use this variable for get the status of the user and filter if the user is
+    // present or is gone
+    let chat_member_status = chat_member.old_chat_member;
+
+    // We converts the user_id to i64 type
     let ChatId(user_id) = ChatId::from(user_id);
-    let mention = user.mention().unwrap_or_else(|| html::user_mention(user_id, user.full_name().as_str()));
 
-    bot.send_message(chat_member.chat.id, format!("bienvenido a {telegram_group_name} {mention}!"))
-        .parse_mode(ParseMode::Html)
-        .await?;
+    // We get the full_name of the user via `mention()` method and we use
+    // `unwrap_or_else` for get the first_name via `full_name` method
+    // if the user don't have a username
+    let username = user.mention().unwrap_or_else(|| html::user_mention(user_id, user.full_name().as_str()));
 
-    Ok(())
-}
+    if chat_member_status.is_present() {
+        bot.send_message(chat_member.chat.id, format!("Hasta pronto {username}!")).await?;
+    }
 
-async fn chat_member_goodbye(bot: Bot, chat_member: ChatMemberUpdated) -> MemberResult {
-    let first_name = chat_member.old_chat_member.user.first_name;
-    let username = chat_member.old_chat_member.user.username.unwrap_or_else(|| first_name.clone());
-    let user_id = chat_member.old_chat_member.user.id;
-    let mention = UserIdMention::mention_user(user_id, username.as_str());
-    //let username = format!("<a href=\"tg://user?id={user_id}\">{username}</a>");
-
-    bot.send_message(chat_member.chat.id, format!("Hasta pronto {mention}!"))
-        .parse_mode(ParseMode::Html)
-        .await?;
+    if !chat_member_status.is_present() {
+        bot.send_message(chat_member.chat.id, format!("Bienvenido a {telegram_group_name} {username}!")).await?;
+    }
 
     Ok(())
 }
